0f622336dc7303d5a383ab8955c816d523c00aa5
Elliot Metsger <emetsger@jhu.edu>
2018-04-05 17:50:39 -0400

    ResourceBuilder javadoc

--- nihms-assembler-api/src/main/java/org/dataconservancy/nihms/assembler/ResourceBuilder.java
+++ nihms-assembler-api/src/main/java/org/dataconservancy/nihms/assembler/ResourceBuilder.java
@@ -16,18 +16,56 @@
 package org.dataconservancy.nihms.assembler;
 
 /**
+ * Allows for various components to contribute to the state of {@link PackageStream.Resource}s without the requirement
+ * to share knowledge of the underlying implementation.
+ *
  * @author Elliot Metsger (emetsger@jhu.edu)
  */
 public interface ResourceBuilder {
 
+    /**
+     * Adds a {@link PackageStream.Resource#checksum()} of the {@code Resource}.  The first
+     * checksum added will be considered the "primary" checksum, returned in response to {@link
+     * PackageStream.Resource#checksum()}.
+     *
+     * @param checksum a checksum of the resource
+     * @return this builder
+     * @see PackageStream.Resource#checksum()
+     */
     ResourceBuilder checksum(PackageStream.Checksum checksum);
 
+    /**
+     * Sets the {@link PackageStream.Resource#mimeType()} of the {@code Resource}.
+     *
+     * @param mimeType the mime type of the resource
+     * @return this builder
+     * @see PackageStream.Resource#mimeType()
+     */
     ResourceBuilder mimeType(String mimeType);
 
+    /**
+     * Sets the {@link PackageStream.Resource#name()} of the {@code Resource}, suitable for use as a filename.
+     *
+     * @param name the resource name
+     * @return this builder
+     * @see PackageStream.Resource#name()
+     */
     ResourceBuilder name(String name);
 
+    /**
+     * Sets the {@link PackageStream.Resource#sizeBytes()} of the {@code Resource}.
+     *
+     * @param sizeBytes the size of the resource, in bytes
+     * @return this builder
+     * @see PackageStream.Resource#sizeBytes()
+     */
     ResourceBuilder sizeBytes(long sizeBytes);
 
+    /**
+     * Builds the Resource object from the state set on this builder.
+     *
+     * @return the Resource object
+     */
     PackageStream.Resource build();
 
 }
