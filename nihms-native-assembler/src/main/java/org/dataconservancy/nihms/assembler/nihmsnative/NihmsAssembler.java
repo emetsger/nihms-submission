ce8a6eb67edfa6e63af6c7ae2388eda6e07c08d1
Elliot Metsger <emetsger@jhu.edu>
2018-04-06 11:15:41 -0400

    Update NihmsAssembler to extend from the AbstractAssembler.  The Manifest and Metadata serializers are injected on construction.

--- nihms-native-assembler/src/main/java/org/dataconservancy/nihms/assembler/nihmsnative/NihmsAssembler.java
+++ nihms-native-assembler/src/main/java/org/dataconservancy/nihms/assembler/nihmsnative/NihmsAssembler.java
@@ -16,121 +16,27 @@
 
 package org.dataconservancy.nihms.assembler.nihmsnative;
 
-import org.apache.commons.io.IOUtils;
-import org.dataconservancy.nihms.assembler.Assembler;
+import org.dataconservancy.nihms.assembler.MetadataBuilder;
 import org.dataconservancy.nihms.assembler.PackageStream;
-import org.dataconservancy.nihms.model.NihmsFile;
-import org.dataconservancy.nihms.model.NihmsSubmission;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 
-import java.io.ByteArrayInputStream;
-import java.net.MalformedURLException;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class NihmsAssembler implements Assembler {
+public class NihmsAssembler extends AbstractAssembler {
 
-    private static final String ERR_MAPPING_LOCATION = "Unable to resolve the location of a submitted file ('%s') to a Spring Resource type.";
+    private StreamingSerializer metadataSerializer;
 
-    private static final String FILE_PREFIX = "file:";
+    private StreamingSerializer manifestSerializer;
 
-    private static final String CLASSPATH_PREFIX = "classpath:";
+    public NihmsAssembler(MetadataBuilderFactory mbf, ResourceBuilderFactory rbf, StreamingSerializer metadataSerializer, StreamingSerializer manifestSerializer) {
+        super(mbf, rbf);
+        this.metadataSerializer = metadataSerializer;
+        this.manifestSerializer = manifestSerializer;
+    }
 
-    private static final String WILDCARD_CLASSPATH_PREFIX = "classpath*:";
-
-    private static final String HTTP_PREFIX = "http:";
-
-    private static final String HTTPS_PREFIX = "https:";
-
-    /**
-     * Assembles Java {@code Object} references to <em>{@code InputStream}s</em> for each file in the package.  The
-     * references are supplied to the {@code NihmsPackageStream} implementation, which does the heavy lifting of
-     * actually creating a stream for the tar.gz archive.
-     *
-     * @param submission the custodial content being packaged
-     * @return a PackageStream which actually creates the stream for the tar.gz archive
-     */
     @Override
-    public PackageStream assemble(NihmsSubmission submission) {
-
-        // Prepare manifest and a serialization of the manifest
-        StreamingSerializer manifestSerializer = new NihmsManifestSerializer(submission.getManifest());
-        // Prepare metadata and a serialization of the metadata
-        StreamingSerializer metadataSerializer = new NihmsMetadataSerializer(submission.getMetadata());
-        // Locate byte streams for uploaded manuscript and supplemental data
-        List<Resource> fileResources = submission.getFiles()
-                .stream()
-                .map(NihmsFile::getLocation)
-                .map(location -> {
-                            if (location.startsWith(FILE_PREFIX)) {
-                                return new FileSystemResource(location);
-                            }
-                            if (location.startsWith(CLASSPATH_PREFIX) ||
-                                    location.startsWith(WILDCARD_CLASSPATH_PREFIX)) {
-                                if (location.startsWith(WILDCARD_CLASSPATH_PREFIX)) {
-                                    return new ClassPathResource(location.substring(WILDCARD_CLASSPATH_PREFIX.length()));
-                                }
-                                return new ClassPathResource(location.substring(CLASSPATH_PREFIX.length()));
-                            }
-                            if (location.startsWith(HTTP_PREFIX) || location.startsWith(HTTPS_PREFIX)) {
-                                try {
-                                    return new UrlResource(location);
-                                } catch (MalformedURLException e) {
-                                    throw new RuntimeException(e.getMessage(), e);
-                                }
-                            }
-                            if (location.contains("/") || location.contains("\\")) {
-                                // assume it is a file
-                                return new FileSystemResource(location);
-                            }
-
-                            throw new RuntimeException(String.format(ERR_MAPPING_LOCATION, location));
-                        })
-                .collect(Collectors.toList());
-
-        SimpleMetadataImpl md = new SimpleMetadataImpl(sanitize(submission.getName()), -1L);
-
-        return new NihmsPackageStream(manifestSerializer, metadataSerializer, fileResources, md);
-    }
-
-    static String sanitize(String name) {
-        String result = name
-                .chars()
-                .filter(NihmsAssembler::isValidChar)
-                .mapToObj(c -> Character.toString((char)c))
-                .collect(Collectors.joining());
-
-        return result;
-    }
-
-    private static boolean isValidChar(int ch) {
-        int i = ch & 0x0000FFFF;
-
-        // outside of the latin-1 code block
-        if (i >= 0x007f) {
-            return false;
-        }
-
-        // a - z 0x61 - 0x7a
-        if (i >= 0x0061 && i <= 0x007a) {
-            return true;
-        }
-
-        // A - Z 0x41 - 0x5a
-        if (i >= 0x0041 && i <= 0x005a) {
-            return true;
-        }
-
-        // 0 - 9 0x30 - 0x39
-        if (i >= 0x0030 && i <= 0x0039) {
-            return true;
-        }
-
-        // otherwise it's an illegal character inside of the latin-1 code block
-        return false;
+    protected PackageStream createPackageStream(List<Resource> custodialResources, MetadataBuilder mb, ResourceBuilderFactory rbf) {
+        return new NihmsPackageStream(manifestSerializer, metadataSerializer, custodialResources, mb, rbf);
     }
 
 }
